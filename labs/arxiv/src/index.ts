96876712dd478ec6605726dc6bced49bc7a3250d
moons-14 <moons@moons14.com>
2024-10-07 17:44:03 +0900

    change: lab

--- labs/arxiv/src/index.ts
+++ labs/arxiv/src/index.ts
@@ -1,12 +1,22 @@
-import { searchByCategory } from "./search";
+import arxivClient, { or, and, category } from "arxiv-client";
+import { allCategories } from "./categories";
 
 const main = async () => {
-    console.log(await searchByCategory({
-        archive: "hep-ph",
-        category: "hep-ph",
-        year: 2021,
-        month: 8,
-    }));
+    const articles = await arxivClient
+        .query(or(...allCategories.slice(10, 20)))
+        .sortBy("lastUpdatedDate")
+        .sortOrder("descending")
+        .start(0)
+        .maxResults(500)
+        .execute();
+
+    for (const article of articles) {
+        console.log(article.updated, article.primaryCategory);
+    }
+
+    console.log(articles.length);
+    console.log("allCategories", allCategories.length);
+    // console.dir(articles, { depth: null });
 }
 
 main();
