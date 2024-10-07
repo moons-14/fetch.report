3035a9f88278b491cdbb6ea728a46f3a995f652c
moons-14 <moons@moons14.com>
2024-10-07 17:50:41 +0900

    feat:use cron

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -6,4 +6,11 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+const scheduled: ExportedHandlerScheduledHandler = async (event, env, ctx) => {
+  // ctx.waitUntil(doSomeTaskOnASchedule())
+}
+
+export default {
+  fetch: app.fetch,
+  scheduled,
+}
